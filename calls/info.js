1abd7ade91975d9ee0179c905046465c4ecff0b0
Saptadeep Banerjee <69459134+imSanko@users.noreply.github.com>
2022-12-09 22:06:08 +0530

    Update info.js

--- calls/info.js
+++ calls/info.js
@@ -4,7 +4,8 @@ const { MessageEmbed } = require("discord.js")
 module.exports = 
 	{
 	data: new SlashCommandBuilder().setName("info").setDescription("Displays info about the currently playing song"),
-	run: async ({ client, interaction }) => {
+	run: async ({ client, interaction }) => 
+	{
 		const queue = client.player.getQueue(interaction.guildId)
 
 		if (!queue) return await interaction.editReply("There are no songs in the queue")
